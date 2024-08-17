d0ad93cada3e58bc6bb8fc2c2b1eee8b4458aa6a
Juan M. Pintos <166875657+JuanMPintos@users.noreply.github.com>
2024-08-17 14:53:08 +0000

    boton actualizar

--- src/js/component/ListadeTareas.jsx
+++ src/js/component/ListadeTareas.jsx
@@ -1,9 +1,19 @@
 import React from "react";
-import Tarea from "../Tarea";
+import Tarea from "./Tarea";
 
 
 
 const ListadeTareas = ({ listaTareas, setListaTareas }) => {
+
+    const editarTarea = (id, nuevoTexto) => {
+        setListaTareas(listaTareas.map((tarea)=>{
+            if(tarea.id === id) {
+                return {...tarea, label: nuevoTexto}
+            }
+            return tarea;
+        }))
+    }
+
     return (
         <ul className="lista-tareas">
             {
@@ -11,7 +21,7 @@ const ListadeTareas = ({ listaTareas, setListaTareas }) => {
                     ?
                     listaTareas.map((tarea) => {
                         return (
-                            <Tarea key={tarea.id} tarea={tarea}/>
+                            <Tarea key={tarea.id} tarea={tarea} editarTarea={editarTarea}/>
                         )
                     })
                     :
---
+++ src/js/component/Tarea.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+
+
+const Tarea = ({ tarea, editarTarea }) => {
+    const [editandoTarea, setEditandoTarea] = useState(false);
+    const [nuevaTarea, setNuevaTarea] = useState(tarea.label);
+
+    const enviarFormulario = async (e) => {
+        e.preventDefault()
+
+        try {
+            const response = await fetch(`https://playground.4geeks.com/todo/todos/${tarea.id}`, {
+                method: "PUT",
+                body: JSON.stringify({
+                    label: nuevaTarea,
+                    is_done: tarea.is_done
+                }),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+
+            });
+        if (!response.ok) {
+            throw new Error("Error al actualizar la tarea");
+        }
+        const data = await response.json();
+        editarTarea(data.id, data.label);
+
+    } catch (error) {
+    console.error("Error al cargar la lista", error);
+ }
+setEditandoTarea(false);
+}
+return (
+    <li className="lista-tareas_tareas">
+        {editandoTarea === true ?
+            <form className="formulario_editar" onSubmit={enviarFormulario}>
+                <input className="formulario-editat-tarea_input"
+                    type="text"
+                    value={nuevaTarea}
+                    onChange={(e) => { setNuevaTarea(e.target.value) }}
+                >
+
+                </input>
+                <button className="boton_actualizar">Actualizar</button>
+            </form>
+            :
+            tarea.label
+
+        }
+        <div className="lista-tareas_contenedor-botones">
+            <button onClick={() => (index)}>
+                <i className="fas fa-trash"></i>
+            </button>
+            <button onClick={() => (setEditandoTarea(!editandoTarea))}>
+                <i className="fas fa-edit"></i>
+            </button>
+        </div>
+    </li>
+)
+}
+
+export default Tarea;
